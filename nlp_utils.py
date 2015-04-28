6a4c887e052f4314238c3bb6746864976495f38f
Vasilis Hatzopoulos <vasilishatzopoulos@Vasiliss-MacBook-Pro.local>
2015-04-28 09:35:06 +0100

    change npl_utils.py naming convention for functions

--- nlp_utils.py
+++ nlp_utils.py
@@ -2,10 +2,11 @@ import nltk
 import random
 import re
 
-#---------------------preprocessing-tweet-words--------------------------------------
-def processTweet(tweet):
+#---------------------cleaning-tweet-words--------------------------------------
+def clean_tweet(tweet):
 	#convert to lower case:
 	tweet = tweet.lower()
+	#sub url with URL
 	tweet = re.sub('((www\.[\s]+)|(https?://[^\s]+))','URL',tweet)
 	#convert @username to ALT_USER
 	tweet = re.sub('@[^\s]+','ALT_USER',tweet)
@@ -19,18 +20,18 @@ def processTweet(tweet):
 
 #---------------------filtering tweet words--------------------------------------
 #huuungry --> hungry
-def replaceTwoOrMore(s):
+def replace_two_or_more(s):
 	#look for 2 or more repetitions of character and replace with character itself
 	pattern = re.compile(r"(.)\1{1,}",re.DOTALL)
 	return pattern.sub(r"\1\1",s)
-	
-def getStopWordsList(swlFilename):
-	
+
+def get_stop_words_list(swlFilename):
+
 	#read the stop words file and build a list
 	stopWords = []
 	stopWords.append('AT_USER')
 	stopWords.append('URL')
-	
+
 	fp = open(swlFilename,'r')
 	line = fp.readline()
 	while line:
@@ -40,12 +41,12 @@ def getStopWordsList(swlFilename):
 	fp.close()
 	return stopWords
 
-def getFeatureVector(tweet,stopWords):
+def get_feature_vector(tweet,stopWords):
 
 	featureVector = []
 	#split tweet into words
 	words = tweet.split()
-	
+
 	for w in words:
 		#replace two or more
 		w = replaceTwoOrMore(w)
@@ -61,10 +62,10 @@ def getFeatureVector(tweet,stopWords):
 	return featureVector
 
 #function to find the most frequent tags in tagged text
-def findtags(tag_prefix,tagged_tex):
+def find_tags(tag_prefix,tagged_tex):
 	cfd = nltk.ConditionalFreqDist( (tag,word) for (word,tag) in tagged_text if tag.startswith(tag_prefix))
-	return dict( (tag,cfd[tag].keys()[:5]) for tag in cfd.conditions() ) 
-	
+	return dict( (tag,cfd[tag].keys()[:5]) for tag in cfd.conditions() )
+
 def document_features(document,word_features):
 	document_words = set(document)
 	features = {}
